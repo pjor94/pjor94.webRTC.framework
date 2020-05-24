23cf47d3c8aa8646f536c7ef87af4caacf7d60f8
philip <philip.orla@pjor94.it>
2020-05-24 16:37:42 +0200

    Initial Commit

---
+++ WebRTC/WebRTC.h
@@ -0,0 +1,19 @@
+//
+//  WebRTC.h
+//  WebRTC
+//
+//  Created by philip on 24/05/2020.
+//  Copyright © 2020 philip. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WebRTC.
+FOUNDATION_EXPORT double WebRTCVersionNumber;
+
+//! Project version string for WebRTC.
+FOUNDATION_EXPORT const unsigned char WebRTCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebRTC/PublicHeader.h>
+
+
